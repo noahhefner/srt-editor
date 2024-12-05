4ee602f93148914134484e8d98c60577e0587df7
Noah Hefner <nhefner127+github@protonmail.com>
2024-12-05 07:12:24 -0500

    ui

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from srt import SRT
 import os
+from pathlib import Path
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -34,14 +35,45 @@ def get_srt():
 
 @app.route('/srt/save', methods=['POST'])
 def save():
+    """
+    Writes SRT contents to a file.
 
-    data = request.get_json()
+    Request format:
 
-    with open(data.get('path'), 'w') as f:
+    {
+        "path": "/path/to/file.srt",
+        "text": "<srt file contents here>"
+    }
+    """
 
-        f.write(data.get('text'))
+    # Check that request has json. By default a request is considered to include
+    # JSON data if the mimetype is application/json or application/*+json.
+    if not request.is_json:
+        return "Bad Request. Request header is not application/json or application/*+json", 400
+    request_json = request.get_json()
+    if request_json is None:
+        return "Bad Request. Request does not contain json.", 400
 
-    return "Success", 200
+    # Get file path from json
+    path = request_json.get('path')
+    if path is None:
+        return "Bad Request. Request json does not contain file path.", 400
+
+    # Get file contents from json
+    file_contents = request_json.get('text')
+    if file_contents is None:
+        return "Bad Request. Request json does not contain file contents.", 400
+
+    # Ensure file exists
+    file = Path(path)
+    if not file.is_file():
+        return f'Not found. File not found: {path}', 404
+
+    # Write contents to file
+    with open(path, 'w') as f:
+        f.write(file_contents)
+
+    return f'Success. Contents saved to: {path}', 200
 
 @app.route('/editor', methods=['GET'])
 def get_editor_page():
